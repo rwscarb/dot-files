7c9ebb4e99887e908a20c3c5159d907a92e9f180
Ryan Scarbery <ryan.scarbery@gmail.com>
2016-07-14 15:09:19 -0700

    add ideavimrc, zprofile and helper script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git submodule init
+git submodule update
+
+SCRIPT_DIR=$(dirname `readlink -f $0`)
+
+ln -sf $SCRIPT_DIR/aliases $HOME/.aliases
+ln -sf $SCRIPT_DIR/zshrc $HOME/.zshrc
+ln -sf $SCRIPT_DIR/zprofile $HOME/.zprofile
+ln -sf $SCRIPT_DIR/tmux/tmux.conf $HOME/.tmux.conf
+ln -sf $SCRIPT_DIR/tmux $HOME/.tmux
+ln -sf $SCRIPT_DIR/vim/vim $HOME/.vim
+ln -sf $SCRIPT_DIR/vim/vimrc $HOME/.vimrc
+ln -sf $SCRIPT_DIR/oh-my-zsh $HOME/.oh-my-zsh
+ln -sf $SCRIPT_DIR/ideavimrc $HOME/.ideavimrc
